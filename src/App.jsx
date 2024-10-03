f069de8184554921374afbbc38f388fa0e5e1106
Leon <leondavies1997@gmail.com>
2024-10-03 19:40:44 +1300

    feat: GTM INNIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages";
+import Layout from "./components/layout";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Layout>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Layout>
   );
 }
 
